76ead3dcc6c1c3e83adeed34d4ee997e0d4ce01a
osama <osamanazar47@gmail.com>
2023-12-01 15:50:48 -0800

    cycel

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/*
+ * check_cycle - check if there is a cycle in the linked lists
+ * @list: the list
+ * Return: an integer
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *hare, *tortoise;
+	tortoise = list;
+	hare = list;
+
+	while (hare && hare->next)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (tortoise == hare)
+			return (1);
+	}
+	return (0);
+}
