2d39350c19706209dd21681b9ddc6f905f77f783
osama <osamanazar47@gmail.com>
2023-12-03 11:29:00 -0800

    fizz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("fizz", end=' ')
         elif i % 5 == 0:
-            print("buzz", end=' ' if i != 100 else '')
+            print("buzz", end=' ')
         else:
             print(i, end=' ')
