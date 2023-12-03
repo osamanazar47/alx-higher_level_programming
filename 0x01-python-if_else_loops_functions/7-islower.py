18dad5438dcd8b62bacbd0c181c85a35ff9df753
osama <osamanazar47@gmail.com>
2023-12-03 06:00:23 -0800

    lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) in range(97, 123):
-        print("{} is lower".format(c))
+        print("{} => lower".format(c))
         return True
     else:
-        print("{} is upper".format(c))
+        print("{} => upper".format(c))
         return False
