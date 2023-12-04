6ce8a0b0e66903e51608ad62ff871aeec867c8f2
osama <osamanazar47@gmail.com>
2023-12-04 03:35:33 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as modulee
+from add_0 import add
 a = 1
 b = 2
 result = modulee.add(a, b)
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,2 @@
 def add(a, b):
     return a + b
-if __name__ == "__main__":
-    a = 0
-    b = 0
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
