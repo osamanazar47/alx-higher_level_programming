440eb4addeea204dd89448a5b17f7c417b528741
osama <osamanazar47@gmail.com>
2023-12-04 15:42:17 -0800

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    n = 0
-    for i in range(len(my_list)):
-        if n < my_list[i]:
-            n = my_list[i]
-    return n
+    if len(my_list) == 0:
+        return "None"
+    else:
+        n = 0
+        for i in range(len(my_list)):
+            if n < my_list[i]:
+                n = my_list[i]
+        return n
