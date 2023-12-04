bd6161f3a040b54853d14e1e4a0e9bbee3763dfe
osama <osamanazar47@gmail.com>
2023-12-04 04:05:02 -0800

    cal

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/path
+if __name__ == "__main__":
+    import calculator_1 as c
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, c.add(a, b)))
+    print("{} - {} = {}".format(a, b, c.sub(a, b)))
+    print("{} * {} = {}".format(a, b, c.mul(a, b)))
+    print("{} / {} = {}".format(a, b, c.div(a, b)))
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    #the addition function
+    return a + b
+def sub(a, b):
+    #the subtaction function
+    return a - b
+def mul(a, b):
+    #the multiplication function
+    return a * b
+def div(a, b):
+    #the division function
+    return int(a / b)
