4532099d46707ad344ef51df711cbe6e1c1bd2a7
osama <osamanazar47@gmail.com>
2023-12-04 04:27:22 -0800

    cal

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     print("{}".foramt(len(argv)), end='')
     if len(argv) == 1:
-        print("argument:")
+        print("argument" + ":" if len(argv) > 0 else ".")
     else:
-        print("arguments:")
+        print("arguments" + ":" if len(argv) > 0 else ".")
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
