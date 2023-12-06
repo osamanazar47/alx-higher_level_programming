b786770ed2b2c7c58b3152405ce7897b03922b2c
osama <osamanazar47@gmail.com>
2023-12-06 14:35:16 -0800

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary :
+    if key in a_dictionary:
         a_dictionary[key] = value
     else:
-        add_value = {key : value}
+        add_value = {key: value}
         a_dictionary.update(add_value)
     return a_dictionary
