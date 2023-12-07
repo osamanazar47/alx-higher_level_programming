9a579dff29026c87e3f4ded548fb1e8f2cee9643
osama <osamanazar47@gmail.com>
2023-12-07 03:00:55 -0800

    romans

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,14 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    romans = {'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
     result = 0
     for i in range(len(roman_string)):
         current_value = romans[roman_string[i]]
-        next_value = romans[roman_string[i + 1]] if i + 1 < len(roman_string) else 0
+        if i + 1 < len(roman_string):
+            next_value = romans[roman_string[i + 1]]
+        else:
+            next_value = 0
         if current_value < next_value:
             result -= current_value
         else:
