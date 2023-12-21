5b781e62367c37e0557e492d035e275340395418
osamanazar <osamanazar47@gmail.com>
2023-12-21 00:40:09 -0800

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Define class Square
+"""
+
+
+class Square:
+    """A square class with a __init__ method."""
+    def __init__(self, size):
+        """
+        a new Square
+
+        Args:
+           size(int): the size of the square
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -5,3 +5,5 @@ this a project on the concepts of classes and objects in python programming lang
 i will explain each file here
 
 0-square.py: a file that contains an empty class that defines a square
+
+1-square.py: a file that contains a class with a private instance attribute size
