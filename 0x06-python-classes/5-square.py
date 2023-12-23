7e3a66d03fc5f37eee10d6ceb527e0c53e2f0764
osamanazar <osamanazar47@gmail.com>
2023-12-23 04:31:55 -0800

    #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a Square class with getter and setter."""
+
+
+class Square:
+    """A square class."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square class
+
+        Args:
+        size(int): the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get the current value of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets a new size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
