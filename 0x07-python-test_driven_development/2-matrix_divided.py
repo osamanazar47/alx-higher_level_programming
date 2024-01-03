d14265a155f95b14f79d39decab51f4ae7c7e04e
osamanazar <osamanazar47@gmail.com>
2024-01-03 13:48:10 -0800

    fix matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,20 +12,23 @@ def matrix_divided(matrix, div):
        matrix(list of lists): the main matrix which contains elements of integers or floats
        div(int, float): the number by which we will divide the elements of the matrix by
     """
-    def matrix_divided(matrix, div):
-        new_matrix = []
-        if not isinstance(div, (int, float)):
-            raise TypeError('div must be a number')
-        if div == 0:
-            raise ZeroDivisionError('division be zero')
-        for i in range(len(matrix)):
-            if i + 1 < len(matrix):
-                if len(matrix[i]) != len(matrix[i + 1]):
-                    raise TypeError('Each row of the matrix must have the same size')
-            new_row = []
-            for j in range(len(matrix[i])):
-                if not isinstance(matrix[i][j], (int, float)):
-                    raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-                new_row.append(round(matrix[i][j] / div, 2))
-            new_matrix.append(new_row)
-        return new_matrix
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(msg_type)
+    len_row = 0
+    msg_size = "Each row of the matrix must have the same size"
+    for elements in matrix:
+        if not elements or not isinstance(elements, list):
+            raise TypeError(msg_type)
+        if len_row != 0 and len(elements) != len_row:
+            raise TypeError(msg_size)
+        for num in elements:
+            if not type(num) in (int, float):
+                raise TypeError(msg_type)
+        len_row = len(elements)
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return m
