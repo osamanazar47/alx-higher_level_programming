c2473442c12f6e5aef53d1a6130889ab194809e5
osamanazar <osamanazar47@gmail.com>
2024-01-05 00:34:18 -0800

    # rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
             return 0
         return (2 * (self.__width + self.__height))
 
-    def __str__(self):
+    def __str__(self) -> str:
         """presents the rectangle shape with the help of #"""
         if self.__width == 0 or self.__height == 0:
             return ("")
