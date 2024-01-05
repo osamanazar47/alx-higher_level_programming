706edfa85941d66e7eae12b8095a3f5a510ca272
osamanazar <osamanazar47@gmail.com>
2024-01-05 00:03:24 -0800

    area an per

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,4 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        return (2 * (self.__width + self.__height))
