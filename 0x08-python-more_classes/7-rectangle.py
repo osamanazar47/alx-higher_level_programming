4e793ee199aa888a4f823897a121f103d11c93e1
osamanazar <osamanazar47@gmail.com>
2024-01-05 06:21:34 -0800

    symbols

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
     """
     number_of_instances = 0
     print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         """the constructure for the Rectangle class"""
         self.width = width
