25d789273cd383bf2783bbefc0ae0a6f27159093
osamanazar <osamanazar47@gmail.com>
2024-01-08 08:51:47 -0800

    sorted lsit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a MyList class"""
+
+
+class MyList(list):
+    """Initializing a class MyList
+    Usage:
+       Inherits the list class and have a public instance method print_sorted
+       print_sorted(self): prints the list but sorted in an ascending order
+    """
+    def __init__(self):
+        """The constructor"""
+        super().__init__()
+
+    def print_sorted(self):
+        """A public instance method that prints the list but sorted"""
+        sorted_list = sorted(self)
+        print(sorted_list)
