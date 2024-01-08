15440dcec089cc92ec1d0671e6afbabca6abf7d8
osamanazar <osamanazar47@gmail.com>
2024-01-08 00:13:19 -0800

    dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a function lookup(obj)"""
+
+
+def lookup(obj):
+    """
+    A function that returns the list of available attributes and methods of an object
+    Args:
+       obj(object): the object that we will return the list of all available attributes and methods of
+    """
+    return dir(obj)
