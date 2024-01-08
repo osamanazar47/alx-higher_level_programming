bdfe38dfb6cbde68b180388d7cf34a949505f1da
osamanazar <osamanazar47@gmail.com>
2024-01-07 22:24:30 -0800

    name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,7 +18,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
        TypeError: If first_name or last_name is not a string
     """
-    if type(first_name) is not  str:
+    if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
         raise TypeError('last_name must be a string')
