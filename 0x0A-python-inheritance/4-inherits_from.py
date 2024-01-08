4503c0f57d4913e27cb3288ddee3d6e0c11a6cd2
osamanazar <osamanazar47@gmail.com>
2024-01-08 10:15:03 -0800

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    A function that returns True if the object is instance of a class
+    that inherited (directly or indirectly) from the specified
+    class ; otherwise False
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
