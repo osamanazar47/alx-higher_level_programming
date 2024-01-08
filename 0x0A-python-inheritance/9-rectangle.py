7cd6d30581c0d07a7143a3836fa99a267a511c19
osamanazar <osamanazar47@gmail.com>
2024-01-08 12:54:56 -0800

    Rectangle with area

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,5 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """returns an info about the rectangle"""
-        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, 
+                self.__height)
