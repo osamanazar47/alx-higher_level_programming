db152e3486299e5c996826851eefa73bd4053c69
osamanazar <osamanazar47@gmail.com>
2024-01-09 10:18:28 -0800

    list of argv

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
         except FileNotFoundError:
             items = []
         for i in range(1, len(sys.argv)):
-            items.append(sys.argv[i])
+            items.extend(sys.argv[i])
         save_to_json_file(items, "add_item.json")
