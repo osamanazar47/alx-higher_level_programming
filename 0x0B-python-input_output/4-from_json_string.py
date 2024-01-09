023e274f7abfa65548efd008563b3ab37bfe29ea
osamanazar <osamanazar47@gmail.com>
2024-01-09 07:00:58 -0800

    from json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Define a function from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """A function that returns an object represented by JSON string"""
+    return json.loads(my_str)
