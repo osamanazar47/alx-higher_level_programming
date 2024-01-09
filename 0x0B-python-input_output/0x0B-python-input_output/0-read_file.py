0a79fe35ef9fddad67968ff875a06dd6751847e2
osamanazar <osamanazar47@gmail.com>
2024-01-09 05:20:15 -0800

    read

--- 0x0B-python-input_output/0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """Define a function read_file"""
 
 
-def read_file(filename=''):
+def read_file(filename=""):
     """A function that that reads a text file and prints it"""
 
     with open(filename) as f:
