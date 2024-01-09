07b61516d06edd57e64b0cea484d5d9465d49c01
osamanazar <osamanazar47@gmail.com>
2024-01-09 06:03:35 -0800

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Define a function write_file"""
+
+
+def write_file(filename="", text=""):
+    """
+    A function that writes a string to a text file
+    Returns: the number of characters written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        print(f.write(text))
