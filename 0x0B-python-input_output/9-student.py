e4313f7109944d8e922eb04b92cc3c6f513eb6c7
osamanazar <osamanazar47@gmail.com>
2024-01-09 13:16:58 -0800

    dictionary rep

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Creating a class Student"""
+
+
+class Student:
+    """
+    Initializing a class Student
+    with an __init__ method and to_json
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """A method that returns the dic representation"""
+        return self.__dict__
