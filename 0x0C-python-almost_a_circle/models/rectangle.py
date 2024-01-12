b58f19f7635893ead74ed23b3dc3b4588fdc7051
osamanazar <osamanazar47@gmail.com>
2024-01-12 00:32:01 -0800

    display update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,98 @@
 #!/usr/bin/python3
+"""Define class Rectangle that inherits from Base"""
+Base = __import__("Base").Base
+
+
+class Rectangle(Base):
+    """Initializing class Rectangle
+    class Rectangle inherits the Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets a new value for x"""
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle."""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets a new value for the height"""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets a new value for x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets a new value for y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints the Rectangle instance with the character #
+        while taking care of x and y"""
+        for y in range(self.__y):
+            print()
+        for h in range(self.__height):
+            print(" " * self.__x, end='')
+            for w in range(self.__width):
+                print("#", end='')
+            print()
+
+    def __str__(self):
+        """A method that returns a string representation of the class"""
+
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += "(" + str(self.id) + ") "
+        string += "{}/{}".format(str(self.__x), str(self.__y))
+        string += " - " + "{}/{}".format(str(self.__width), str(self.__height))
+        return string
