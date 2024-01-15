9f33fcc01846fcb74ade106bcc56b82cf7a3b826
osamanazar <osamanazar47@gmail.com>
2024-01-15 01:06:50 -0800

    save to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
+        """Return the JSON serialization of a list of dictionaries.
+
+        Args:
+            list_dictionaries(list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        json_rep = cls.to_json_string(list_objs)
+        name = cls.__name__
+        filename = "{}.json".format(name)
+        with open(filename, "w") as f:
+            f.write(json_rep)
