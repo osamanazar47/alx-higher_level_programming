db99ee3816adad497926ec4cbb85809a8f7da783
osamanazar <osamanazar47@gmail.com>
2024-02-14 06:28:07 -0800

    id 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- A script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
 SELECT COUNT(*)
 FROM `first_table`
-WHERE `id` = 98;
+WHERE `id` = 89;
