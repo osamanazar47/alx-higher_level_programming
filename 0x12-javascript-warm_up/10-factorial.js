bad8b9a11d9276b437cf85f0b9d6fe1e7b77cdc3
osamanazar47 <osamanazar47@gmail.com>
2024-03-13 15:14:43 +0200

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+function factorialLoop(number) {
+  let result = 1;
+  for (let i = 2; i <= number; i++) {
+      result *= i;
+  }
+  return result;
+}
+
+console.log(factorialLoop(process.argv[2]));
