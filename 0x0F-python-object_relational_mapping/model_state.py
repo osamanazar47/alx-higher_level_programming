0a64d654cbecdf7c4e0b4a2c38a6ccd59a385e99
osamanazar47 <osamanazar47@gmail.com>
2024-03-16 14:53:04 +0200

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Defining two classes Base and State which inherites from Base
-Base class is an instance of declarative_base()
+Defining two classes Base and State which
+inherites from Base
+- Base class is an instance of declarative_base()
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+
 mymetadata = MetaData()
 base = declarative_base(metadata=mymetadata)
 
