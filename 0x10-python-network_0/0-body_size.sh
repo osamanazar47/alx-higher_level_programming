06a44911dfeadce9fce82a37c63f8090897d6e8f
osamanazar47 <osamanazar47@gmail.com>
2024-03-28 01:53:51 +0200

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!usr/bin/env bash
 #a Bash script that takes in a URL, and gives the size of the response
-curl -w "%content-lenght:" "$1"
+size=$(curl -s -w "%{size_download}" -o /dev/null "$1")
+echo "$size"
