3c75c06ac62d87ea670d0de2f179a03bc8ba9128
osamanazar47 <osamanazar47@gmail.com>
2024-03-28 03:00:06 +0200

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """ a function that finds the peak in a list"""
+
+    if len(list_of_integers) == 0:
+        return None
+
+    peak = 0
+    for i in list_of_integers:
+        if i > peak:
+            peak = i
+    return peak
