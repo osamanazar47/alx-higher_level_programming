240307d3eeae0ca73a7a34af3b62ffec9ca1122a
osamanazar47 <osamanazar47@gmail.com>
2024-03-28 03:05:07 +0200

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,24 +2,21 @@
 """Finds a peak in a list of unsorted integers"""
 
 
-def find_peak(list_of_integers):
+def find_peak(list_of_integers, left=None, right=None):
     """A function that finds the peak in a list using divide and conquer."""
-    if len(list_of_integers) == 0:
-        return None
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(list_of_integers) - 1
 
-    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
-
-
-def find_peak_recursive(arr, left, right):
-    """Recursive helper function to find peak in a list."""
     if left == right:
-        return arr[left]
+        return list_of_integers[left]
 
     mid = (left + right) // 2
 
-    if arr[mid] < arr[mid + 1]:
+    if list_of_integers[mid] < list_of_integers[mid + 1]:
         # If the middle element is less than the next element, peak must be on the right side
-        return find_peak_recursive(arr, mid + 1, right)
+        return find_peak(list_of_integers, mid + 1, right)
     else:
         # Otherwise, peak must be on the left side or at the middle element
-        return find_peak_recursive(arr, left, mid)
+        return find_peak(list_of_integers, left, mid)
