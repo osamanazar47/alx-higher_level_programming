626a69d516482cbae37fdeee9dedf4d1d9594d53
osamanazar47 <osamanazar47@gmail.com>
2024-03-29 23:14:30 +0200

    requests-id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,8 +3,9 @@
 sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header
 """
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-res = requests.get(sys.argv[1])
-print(res.headers['X-Request-Id'])
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
