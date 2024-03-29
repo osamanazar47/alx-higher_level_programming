8d3d25f0c420e1f8564fdd0acd5e304cbe071ec4
osamanazar47 <osamanazar47@gmail.com>
2024-03-29 16:35:36 +0200

    email

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request
+to the URL and displays the body of the response (decoded in utf-8).
+"""
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            content = res.read()
+            print(content)
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
