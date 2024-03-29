a5dad5594d705dc0dfa8015329760f764c85a568
osamanazar47 <osamanazar47@gmail.com>
2024-03-29 16:09:49 +0200

    request_id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
+"""
+A script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response
+"""
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    req_id = response.getheader('X-Request-Id')
-    print(req_id)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        req_id = response.getheader('X-Request-Id')
+        print(req_id)
