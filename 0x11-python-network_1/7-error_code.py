d5298c1297fac6bb8385b057007e96ae26432896
osamanazar47 <osamanazar47@gmail.com>
2024-03-29 23:58:19 +0200

    requests-error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to
+the URL and displays the body of the response (decoded in utf-8).
+manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import requests.exceptions
+
+    try:
+        res = requests.get(sys.argv[1])
+        res.raise_for_status()
+        print(res.content)
+    except requests.exceptions.HTTPError as err:
+        print("Error code:", err)
