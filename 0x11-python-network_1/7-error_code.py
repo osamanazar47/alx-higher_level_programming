c47c975f51d956531ca0a97a260513f6c63e7d24
osamanazar47 <osamanazar47@gmail.com>
2024-03-30 00:03:25 +0200

    requests-error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,8 @@
 """
 a Python script that takes in a URL, sends a request to
 the URL and displays the body of the response (decoded in utf-8).
-manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
+manage urllib.error.HTTPError exceptions and print:
+Error code: followed by the HTTP status code
 """
 
 if __name__ == '__main__':
