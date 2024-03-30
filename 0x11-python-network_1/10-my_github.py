2ed5bb06d7230733a4810986bf77e7eb0f6ade79
osamanazar47 <osamanazar47@gmail.com>
2024-03-30 02:14:19 +0200

    github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    res = requests.get('https://api.github.com/user', auth=(username, password))
+    json_data = res.json()
+    print(json_data['id'])
