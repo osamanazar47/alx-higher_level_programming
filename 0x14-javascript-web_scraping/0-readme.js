1b0b2b9f5c88bb6cc23c89296698c14946c3b5b8
osamanazar47 <osamanazar47@gmail.com>
2024-06-21 14:47:54 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err); // Print the error object if reading fails
+      return;
+    }
+    console.log(data); // Print the file content if reading is successful
+});
